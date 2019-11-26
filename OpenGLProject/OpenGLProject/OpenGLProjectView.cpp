30ff55cab334360b31a139199b5bdaac9ad2cf2b
SazLaptop <sazfirst@gmail.com>
2019-11-26 14:49:12 +0900

    한글깨짐 수정

--- OpenGLProject/OpenGLProject/OpenGLProjectView.cpp
+++ OpenGLProject/OpenGLProject/OpenGLProjectView.cpp
@@ -1,7 +1,7 @@
-﻿癤�#include "pch.h"
+﻿#include "pch.h"
 #include "framework.h"
-// SHARED_HANDLERS�뒗 誘몃━ 蹂닿린, 異뺤냼�뙋 洹몃┝ 諛� 寃��깋 �븘�꽣 泥섎━湲곕�� 援ы쁽�븯�뒗 ATL �봽濡쒖젥�듃�뿉�꽌 �젙�쓽�븷 �닔 �엳�쑝硫�
-// �빐�떦 �봽濡쒖젥�듃��� 臾몄꽌 肄붾뱶瑜� 怨듭쑀�븯�룄濡� �빐 以띾땲�떎.
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
 #ifndef SHARED_HANDLERS
 #include "OpenGLProject.h"
 #endif
@@ -14,7 +14,7 @@
 #endif
 IMPLEMENT_DYNCREATE(COpenGLProjectView, CView)
 BEGIN_MESSAGE_MAP(COpenGLProjectView, CView)
-	// �몴以� �씤�뇙 紐낅졊�엯�땲�떎.
+	// 표준 인쇄 명령입니다.
 	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
 	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
 	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &COpenGLProjectView::OnFilePrintPreview)
@@ -22,7 +22,7 @@ BEGIN_MESSAGE_MAP(COpenGLProjectView, CView)
 	ON_WM_CREATE()
 	ON_WM_DESTROY()
 	ON_WM_SIZE()
-	// �쑀���媛� 異붽���븳 硫붿떆吏� �엯�땲�떎
+	// 유저가 추가한 메시지 입니다
 	ON_MESSAGE(UWM_CUSTOM1, &COpenGLProjectView::OnUwmCustom1)
 	ON_MESSAGE(UWM_CHECKED, &COpenGLProjectView::OnUwmChecked)
 	ON_WM_RBUTTONDOWN()
@@ -67,12 +67,12 @@ bool COpenGLProjectView::SetDevicePixelFormat(HDC hdc) {
 	return TRUE;
 }
 
-// COpenGLProjectView �깮�꽦/�냼硫�
+// COpenGLProjectView 생성/소멸
 COpenGLProjectView::COpenGLProjectView() noexcept { }
 COpenGLProjectView::~COpenGLProjectView() {}
 BOOL COpenGLProjectView::PreCreateWindow(CREATESTRUCT& cs) { return CView::PreCreateWindow(cs); }
 
-// COpenGLProjectView 洹몃━湲�
+// COpenGLProjectView 그리기
 void COpenGLProjectView::OnDraw(CDC* /*pDC*/)
 {
 	COpenGLProjectDoc* pDoc = GetDocument();
@@ -80,12 +80,12 @@ void COpenGLProjectView::OnDraw(CDC* /*pDC*/)
 	if (!pDoc)
 		return;
 
-	// TODO: �뿬湲곗뿉 �썝�떆 �뜲�씠�꽣�뿉 ����븳 洹몃━湲� 肄붾뱶瑜� 異붽���빀�땲�떎.
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
 	DrawGLScene();
 	Invalidate(FALSE);
 }
 
-// COpenGLProjectView �씤�뇙
+// COpenGLProjectView 인쇄
 void COpenGLProjectView::OnFilePrintPreview()
 {
 #ifndef SHARED_HANDLERS
@@ -94,16 +94,16 @@ void COpenGLProjectView::OnFilePrintPreview()
 }
 BOOL COpenGLProjectView::OnPreparePrinting(CPrintInfo* pInfo)
 {
-	// 湲곕낯�쟻�씤 以�鍮�
+	// 기본적인 준비
 	return DoPreparePrinting(pInfo);
 }
 void COpenGLProjectView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
 {
-	// TODO: �씤�뇙�븯湲� �쟾�뿉 異붽�� 珥덇린�솕 �옉�뾽�쓣 異붽���빀�땲�떎.
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
 }
 void COpenGLProjectView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
 {
-	// TODO: �씤�뇙 �썑 �젙由� �옉�뾽�쓣 異붽���빀�땲�떎.
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
 }
 void COpenGLProjectView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
 {
@@ -119,7 +119,7 @@ void COpenGLProjectView::OnDestroy()
 	wglDeleteContext(m_hglRC);
 }
 
-// COpenGLProjectView 吏꾨떒
+// COpenGLProjectView 진단
 #ifdef _DEBUG
 void COpenGLProjectView::AssertValid() const { CView::AssertValid(); }
 void COpenGLProjectView::Dump(CDumpContext& dc) const { CView::Dump(dc); }
@@ -135,7 +135,7 @@ int COpenGLProjectView::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	if (CView::OnCreate(lpCreateStruct) == -1)
 		return -1;
 
-	// TODO:  �뿬湲곗뿉 �듅�닔�솕�맂 �옉�꽦 肄붾뱶瑜� 異붽���빀�땲�떎.
+	// TODO:  여기에 특수화된 작성 코드를 추가합니다.
 	m_hDC = GetDC()->m_hDC;
 
 	if (!SetDevicePixelFormat(m_hDC)) {
@@ -152,10 +152,10 @@ int COpenGLProjectView::OnCreate(LPCREATESTRUCT lpCreateStruct)
 
 void COpenGLProjectView::initGL()
 {
-	TRACE0("initGL �떆�옉\n");
+	TRACE0("initGL 시작\n");
 	cameraController.AttachTarget(&camera);
 
-	// GLEW瑜� �궗�슜�븯湲� �쟾�뿉 癒쇱�� 珥덇린�솕�빀�땲�떎
+	// GLEW를 사용하기 전에 먼저 초기화합니다
 	GLenum err = glewInit();
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glClearDepth(1.0f);
@@ -166,16 +166,14 @@ void COpenGLProjectView::initGL()
 	glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
 	glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
 
-
-
-	// Shader 媛앹껜瑜� �깮�꽦�빀�땲�떎. �씤�옄濡� �꽆寃⑥＜�뒗 string�뿉 �빐�떦�븯�뒗 glsl�뙆�씪�쓣 �뎽�씠�뜑濡� �궗�슜�빀�땲�떎
+	// Shader 객체를 생성합니다. 인자로 넘겨주는 string에 해당하는 glsl파일을 쉐이더로 사용합니다
 	glslShader = Shader("LightingVertexShader.glsl", "LightingFragmentShader.glsl");
-	// �빐�떦�븯�뒗 �뎽�씠�뜑瑜� �궗�슜�븯�젮硫� 諛섎뱶�떆 �샇異쒗빐�빞 �빀�땲�떎
+	// 해당하는 쉐이더를 사용하려면 반드시 호출해야 합니다
 	glslShader.use();
 	/*
-		吏��젙�븳 寃쎈줈�뿉 �엳�뒗 .obj �뙆�씪 �븯�굹瑜� Object 媛앹껜濡� 留뚮뱾�뼱�꽌 ����옣�빀�땲�떎
-		Object 媛앹껜濡� ����옣�떆�뿉 洹� 媛앹껜媛� �궗�슜�븷 Shader瑜� �씤�옄濡� �꽆寃⑥빞 �빀�땲�떎
-		����옣�븳 Object 媛앹껜瑜� ObjectManager �겢�옒�뒪�쓽 map�뿉 吏묒뼱�꽔�뒿�땲�떎
+		지정한 경로에 있는 .obj 파일 하나를 Object 객체로 만들어서 저장합니다
+		Object 객체로 저장시에 그 객체가 사용할 Shader를 인자로 넘겨야 합니다
+		저장한 Object 객체를 ObjectManager 클래스의 map에 집어넣습니다
 	*/
 	ObjectManager::LoadObject(glslShader, "../OpenGLProject/Asset/IronMan.obj");
 	// ObjectManager::LoadObject(glslShader, "../OpenGLProject/Asset/Kizuna/kizunaai.obj");
@@ -183,55 +181,55 @@ void COpenGLProjectView::initGL()
 	// ObjectManager::LoadObject(glslShader, "../OpenGLProject/Asset/h/Handgun.obj");
 
 	/*
-		Light 媛앹껜瑜� �깮�꽦�빀�땲�떎 LightingFragmentShader瑜� �궗�슜�빐�빞 �쟻�슜�맗�땲�떎
-		�궗�슜�븷 �뎽�씠�뜑, �쐞移�, ambient, diffuse, specular 媛믪쓣 �씤�옄濡� �꽆源곷땲�떎
+		Light 객체를 생성합니다 LightingFragmentShader를 사용해야 적용됩니다
+		사용할 쉐이더, 위치, ambient, diffuse, specular 값을 인자로 넘깁니다
 	*/
 	light0 = Light(glslShader, 0, 100, 50, glm::vec3(1, 1, 1), glm::vec3(1, 1, 1), glm::vec3(1, 1, 1));
 
-	// Skybox Shader 諛� 媛앹껜 �깮�꽦
+	// Skybox Shader 및 객체 생성
 	{
-		// skybox留� �젋�뜑留곹븯�뒗 Shader瑜� �깮�꽦�빀�땲�떎
+		// skybox만 렌더링하는 Shader를 생성합니다
 		skyboxShader = Shader("SkyboxVS.glsl", "SkyboxFS.glsl");
 		skyboxShader.use();
-		// �뤃�뜑�뿉 �엳�뒗 �뀓�뒪泥섎�� �궗�슜�븯�뿬 Skybox 媛앹껜瑜� 留뚮벊�땲�떎
+		// 폴더에 있는 텍스처를 사용하여 Skybox 객체를 만듭니다
 		skybox0 = Skybox(skyboxShader, "../OpenGLProject/Asset/Skyboxes/BerzeliiPark");
 	}
 
-	TRACE0("濡쒕뵫 醫낅즺\n");
+	TRACE0("로딩 종료\n");
 }
 
-// OnCreate �씠�썑�뿉 �쟻�뼱�룄 �븳踰� �샇異쒕맗�땲�떎, �솕硫댁쓽 �겕湲곌�� 蹂��븯硫� �샇異쒕맗�땲�떎
+// OnCreate 이후에 적어도 한번 호출됩니다, 화면의 크기가 변하면 호출됩니다
 void COpenGLProjectView::OnSize(UINT nType, int cx, int cy)
 {
 	CView::OnSize(nType, cx, cy);
 
-	// TODO: �뿬湲곗뿉 硫붿떆吏� 泥섎━湲� 肄붾뱶瑜� 異붽���빀�땲�떎.
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
 	ReSizeGLScene(cx, cy);
 }
-// OnSize�뿉�꽌 �샇異쒕맗�땲�떎, �솕硫댁쓽 �겕湲곌�� 蹂��븯硫� �샇異쒕맗�땲�떎
+// OnSize에서 호출됩니다, 화면의 크기가 변하면 호출됩니다
 void COpenGLProjectView::ReSizeGLScene(GLsizei width, GLsizei height)
 {
 	// don't want a divide by zero
 	if (height == 0)
 		height = 1;
 
-	glViewport(0, 0, width, height); // MFC �쟾泥� 李쎌쓽 �겕湲곌�� 蹂��븯硫� 洹몄뿉 留욎떠�꽌 OpenGL濡� 洹몃┫ �솕硫댁쓽 �겕湲곕룄 蹂��솕�떆�궢�땲�떎
+	glViewport(0, 0, width, height); // MFC 전체 창의 크기가 변하면 그에 맞춰서 OpenGL로 그릴 화면의 크기도 변화시킵니다
 	/*
-		Viewport�쓽 �겕湲곌�� 蹂��뻽�쑝誘�濡� 洹몄뿉 留욎떠�꽌 移대찓�씪媛� 李띿쓣 Projection matrix�룄 �깉濡� 怨꾩궛�빀�땲�떎
-		怨꾩궛�븳 Projection maxtrix瑜� �쁽�옱 �궗�슜以묒씤 Shader�뿉�꽌 Uniform mat4濡� 吏��젙�맂 projection 蹂��닔�뿉 �쟾�떖�빀�땲�떎
-		Shader�쓽 projection Uniform 蹂��닔�뒗 VertexShader�뿉 �엳�뒿�땲�떎
+		Viewport의 크기가 변했으므로 그에 맞춰서 카메라가 찍을 Projection matrix도 새로 계산합니다
+		계산한 Projection maxtrix를 현재 사용중인 Shader에서 Uniform mat4로 지정된 projection 변수에 전달합니다
+		Shader의 projection Uniform 변수는 VertexShader에 있습니다
 	*/
 	mat4 projection = perspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 1000.0f);
 	glslShader.setMatrix4(projection, "projection");
 	skyboxShader.setMatrix4(projection, "u_Projection");
 }
 
-// 二쇨린�쟻�쑝濡� �샇異쒕릺�뒗 �븿�닔�엯�땲�떎 �뿬湲곗뿉 洹몃┝�쓣 洹몃┰�땲�떎
+// 주기적으로 호출되는 함수입니다 여기에 그림을 그립니다
 void COpenGLProjectView::DrawGLScene(void)
 {
 	/*
-		Controller�뒗 �봽�젅�엫媛� �떆媛꾩감瑜� 怨꾩궛�빀�땲�떎
-		移대찓�씪�쓽 ���吏곸엫�룄 �뿬湲곗꽌 �쟻�슜�릺誘�濡� �젣�씪 癒쇱�� �샇異쒗빐�빞 �떎
+		Controller는 프레임간 시간차를 계산합니다
+		카메라의 움직임도 여기서 적용되므로 제일 먼저 호출해야 다
 	*/
 	_SystemMangement_::Clock();
 	auto cinfo = cameraController.GetControl_info();
@@ -244,33 +242,33 @@ void COpenGLProjectView::DrawGLScene(void)
 	glDepthFunc(GL_LESS);
 	glslShader.use();
 	/*
-		移대찓�씪�쓽 �쐞移섏�� 媛곷룄�뿉 �뵲瑜� local 醫뚰몴怨꾨�� world 醫뚰몴怨꾨줈 蹂��솚�븳 matrix瑜� 怨꾩궛�빀�땲�떎
-		Controller �겢�옒�뒪�뿉�꽌 留덉슦�뒪, �궎蹂대뱶 �엯異쒕젰�뿉 �뵲�씪 Camera �겢�옒�뒪�쓽 �쐞移섍컪, �젙硫� 踰≫꽣 媛믪쓣 蹂��솕�떆�궢�땲�떎
-		�뿬湲곗뿉�꽌 View Matrix瑜� 怨꾩궛�븯怨� �쁽�옱 �궗�슜以묒씤 Shader�뿉�꽌 Uniform mat4濡� 吏��젙�맂 view 蹂��닔�뿉 �쟾�떖�빀�땲�떎
-		Shader�쓽 view Uniform 蹂��닔�뒗 VertexShader�뿉 �엳�뒿�땲�떎
+		카메라의 위치와 각도에 따른 local 좌표계를 world 좌표계로 변환한 matrix를 계산합니다
+		Controller 클래스에서 마우스, 키보드 입출력에 따라 Camera 클래스의 위치값, 정면 벡터 값을 변화시킵니다
+		여기에서 View Matrix를 계산하고 현재 사용중인 Shader에서 Uniform mat4로 지정된 view 변수에 전달합니다
+		Shader의 view Uniform 변수는 VertexShader에 있습니다
 	*/
 	mat4 view = camera.getViewMatrix();
 	glslShader.setMatrix4(view, "view");
 
 	/*
-		ObjectManager �겢�옒�뒪�뒗 Object 媛앹껜�뱾�쓣 map�뿉�떎媛� ����옣�빐�끉 �긽�깭�엯�땲�떎
-		����옣�맂 Object 媛앹껜�뱾�쓣 李⑤�����濡� 洹몃┰�땲�떎
-		�궗�슜�븷 Shader瑜� �씤�옄濡� �꽆源곷땲�떎
+		ObjectManager 클래스는 Object 객체들을 map에다가 저장해논 상태입니다
+		저장된 Object 객체들을 차례대로 그립니다
+		사용할 Shader를 인자로 넘깁니다
 	*/
 	ObjectManager::DrawObjects(glslShader);
 
-	glDepthFunc(GL_LEQUAL); // Object媛� 洹몃젮吏�吏� �븡��� 遺�遺꾩뿉 Skybox媛� 洹몃젮吏묐땲�떎
-	skyboxShader.use(); // �뼱�뒓 Shader瑜� �궗�슜�빐�꽌 洹몃┫嫄댁�� �꽕�젙�빀�땲�떎
-	view = glm::mat4(glm::mat3(camera.getViewMatrix())); // View�쓽 �씠�룞��� �쟻�슜�븯吏� �븡湲� �쐞�빐 w�슂�냼瑜� �젣嫄고빀�땲�떎
+	glDepthFunc(GL_LEQUAL); // Object가 그려지지 않은 부분에 Skybox가 그려집니다
+	skyboxShader.use(); // 어느 Shader를 사용해서 그릴건지 설정합니다
+	view = glm::mat4(glm::mat3(camera.getViewMatrix())); // View의 이동은 적용하지 않기 위해 w요소를 제거합니다
 	skyboxShader.setMatrix4(view, "u_View");
-	skybox0.Draw(); // �빐�떦 skybox瑜� 洹몃┰�땲�떎
+	skybox0.Draw(); // 해당 skybox를 그립니다
 
 	glslShader.use();
 	// swap buffer
 	SwapBuffers(m_hDC);
 }
 
-// MFC�쓽 �냽�꽦李쎌뿉�꽌 �궇�씪�삤�뒗 硫붿떆吏��엯�땲�떎 李⑦썑�뿉 �븘�슂�븯硫� 援ы쁽�빀�땲�떎
+// MFC의 속성창에서 날라오는 메시지입니다 차후에 필요하면 구현합니다
 afx_msg LRESULT COpenGLProjectView::OnUwmCustom1(WPARAM wParam, LPARAM lParam)
 {
 	//CString msg = _T("");
@@ -282,12 +280,12 @@ afx_msg LRESULT COpenGLProjectView::OnUwmChecked(WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
-// 留덉슦�뒪 諛� �궎蹂대뱶 �엯異쒕젰��� OpenGLProjectView.cpp�뿉�꽌 諛쏆븘�꽌 Controller �겢�옒�뒪濡� �꽆湲곌퀬 Controller �겢�옒�뒪�뿉�꽌 泥섎━�빀�땲�떎
+// 마우스 및 키보드 입출력은 OpenGLProjectView.cpp에서 받아서 Controller 클래스로 넘기고 Controller 클래스에서 처리합니다
 void COpenGLProjectView::OnRButtonDown(UINT nFlags, CPoint point) { cameraController.OnRButtonDown(nFlags, point); }
 void COpenGLProjectView::OnRButtonUp(UINT nFlags, CPoint point)
 {
 	cameraController.OnRButtonUp(nFlags, point);
-	// �슦�겢由� �셿猷뚯떆 踰쀬뼱�굹�젮硫� 二쇱꽍�쓣 吏��썙�빞�븳�떎
+	// 우클릭 완료시 벗어나려면 주석을 지워야한다
 	// ClientToScreen(&point);
 	// OnContextMenu(this, point);
 }
